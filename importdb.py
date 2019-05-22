8b03bad6f1c1c9447a5e3b8c5068265777d71fb0
Simon Clemente <serin113@users.noreply.github.com>
2019-05-22 14:59:57 +0800

    Print db.conf contents in importdb.py

--- importdb.py
+++ importdb.py
@@ -14,9 +14,11 @@ if os.environ.get('DYNO') is not None:
     db_conf = open("db.conf", "w")
     db_conf.write("[connector_python]\n")
     db_conf.write("host = {}\ndatabase = {}\nuser = {}\npassword = {}\nport = {}\nraise_on_warnings = True\n".format(url.hostname, url.path[1:], url.username, url.password, 3306))
-    print("host = {}\ndatabase = {}\nuser = {}\npassword = {}\nport = {}\nraise_on_warnings = True\n".format(url.hostname, url.path[1:], url.username, url.password, 3306))
     db_conf.close()
     
+    test = open("db.conf", "r")
+    print(test.read())
+    test.close()
     print("Creating database connection")
     # create database connection
     dbc = modules.DBConnection("db.conf")
