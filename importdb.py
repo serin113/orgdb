9857218078d7a0eba2c32620acd95de136b1569f
Simon Clemente <serin113@users.noreply.github.com>
2019-05-22 14:53:56 +0800

    Print credentials when running importdb.py

--- importdb.py
+++ importdb.py
@@ -14,6 +14,7 @@ if os.environ.get('DYNO') is not None:
     db_conf = open("db.conf", "w")
     db_conf.write("[connector_python]\n")
     db_conf.write("host = {}\ndatabase = {}\nuser = {}\npassword = {}\nport = {}\nraise_on_warnings = True\n".format(url.hostname, url.path[1:], url.username, url.password, 3306))
+    print("host = {}\ndatabase = {}\nuser = {}\npassword = {}\nport = {}\nraise_on_warnings = True\n".format(url.hostname, url.path[1:], url.username, url.password, 3306))
     db_conf.close()
     
     print("Creating database connection")
